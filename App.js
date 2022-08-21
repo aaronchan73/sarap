37b014facd427eb9bf4d9e91cb6c821f5474c072
Aaron Chan <84891329+aaronchan73@users.noreply.github.com>
2022-08-20 18:07:24 -0700

    Set up react nav

--- App.js
+++ App.js
@@ -1,14 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import GroceryList from "./components/GroceryList";
 import Splash from "./components/Splash";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Splash />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="GroceryList" component={GroceryList} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <Splash />
+    //   <StatusBar style="auto" />
+    // </View>
   );
 }
 
