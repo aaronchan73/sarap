c2dd88e94cbd049f4dd7f4634307c89dddc19062
Aaron Chan <84891329+aaronchan73@users.noreply.github.com>
2022-08-22 17:44:02 -0700

    Added marker as test

--- components/Map.js
+++ components/Map.js
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
 import { View } from "react-native";
-import MapView from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 import styles from "./Styles";
 
 const Map = () => {
   const [region] = useState({
-    latitude: 37.78825,
-    longitude: -122.4324,
+    latitude: 49.267665596,
+    longitude: -123.241999032,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   });
 
   return (
     <View style={styles.map_container}>
-      <MapView initialRegion={region} style={styles.map} />
+      <MapView initialRegion={region} style={styles.map}>
+        <Marker
+          coordinate={{
+            latitude: 49.267665596,
+            longitude: -123.241999032,
+          }}
+          title="UBC"
+          description="You are here."
+        />
+      </MapView>
     </View>
   );
 };
