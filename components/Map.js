f4806d49c9d421e232388270b928715422d187a7
Aaron Chan <chan.aaron73@gmail.com>
2022-08-25 20:24:12 -0700

    Added geolocation

--- components/Map.js
+++ components/Map.js
@@ -1,19 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import MapView, { Marker } from "react-native-maps";
 import styles from "./Styles";
+import Geolocation from "react-native-geolocation-service";
 
 const Map = () => {
-  const [region] = useState({
-    latitude: 49.267665596,
-    longitude: -123.241999032,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421,
-  });
+  const [region, setRegion] = useState({});
+  const delta = { latitudeDelta: 0.0922, longitudeDelta: 0.0421 };
+
+  useEffect(() => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position);
+        console.log({
+          latitude: position["coords"]["latitude"],
+          longitude: position["coords"]["longitude"],
+          ...delta,
+        });
+        setRegion({
+          latitude: position["coords"]["latitude"],
+          longitude: position["coords"]["longitude"],
+          ...delta,
+        });
+      },
+      (error) => {
+        // See error code charts below.
+        console.log(error.code, error.message);
+      },
+      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
+    );
+  }, []);
 
   return (
     <View style={styles.map_container}>
-      <MapView initialRegion={region} style={styles.map}>
+      <MapView region={region} style={styles.map}>
         <Marker
           coordinate={{
             latitude: 49.267665596,
--- components/Splash.js
+++ components/Splash.js
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, Image, Button } from "react-native";
 import splashArt from "../assets/splash_art.png";
 import styles from "./Styles";
+import Geolocation from "react-native-geolocation-service";
 
 const Home = ({ navigation }) => {
+  useEffect(() => {
+    Geolocation.requestAuthorization("whenInUse");
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Sarap</Text>
