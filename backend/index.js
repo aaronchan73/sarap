59056aacfd1250b8241d053e105034145b07e756
Aaron Chan <chan.aaron73@gmail.com>
2022-09-01 15:59:48 -0700

    index edits

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+
+const express = require("express");
+const mysql = require("mysql");
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.PASS,
+  database: "sarapdb",
+});
+
+connection.connect();
+
+connection.query("SELECT 1 + 1 AS solution", (err, rows, fields) => {
+  if (err) throw err;
+
+  console.log("The solution is: ", rows[0].solution);
+});
+
+connection.end();
